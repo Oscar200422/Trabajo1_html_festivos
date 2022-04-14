e6192dec72c2ccde94337ee0cc5a74a4e2940af2
Oscar200422 <102707312+Oscar200422@users.noreply.github.com>
2022-04-14 11:53:47 -0500

    Actividad front

---
+++ Actividad front/Front.js
@@ -0,0 +1,13 @@
+var edad = prompt("Ingrese edad");
+if (edad ==18){
+    alert("Ingreso permitido, cumples con los requisitos")
+    
+
+}
+if(edad >= 50){
+    alert("Ingreso permitido ")
+}
+     else
+     if  (edad<=17){
+    alert("Eres ilegal, no puedes estar aquí")
+     }
