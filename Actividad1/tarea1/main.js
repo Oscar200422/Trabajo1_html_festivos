83a8f9c09302f9505316a394dd36963552be0699
Oscar200422 <102707312+Oscar200422@users.noreply.github.com>
2022-04-16 18:00:18 -0500

    Trabajos primer taller

--- Actividad1/tarea1/main.js
+++ Actividad1/tarea1/main.js
@@ -8,22 +8,32 @@ const toggleMenu = () =>{
     enlaces.classList.toggle("enlaces2");
     enlaces.style.transition ="transform 0.5s ease-in-out";
 }
+
 icon.addEventListener("click", function(){
     toggleMenu();
-    if (document.querySelector(".enlaces.enlaces2")){
+    if(document.querySelector("enlaces.emlaces2")){
         abierto=true;
-    }
-    else{
+    }else{
         abierto=false;
     }
 })
+
 window.addEventListener("click", function(e){
-    this.crossOriginIsolated.log(e.target)
+this.console.log(e.target)
+if (abierto){
     if(e.target !== menuicon){
         toggleMenu();
         abierto=false
+        }
     }
-    else {
-        abierto=true
+}) 
+
+window.addEventListener("resize", function(){
+if(this.screen.width>800){
+    if(abierto){
+        toggleMenu();
+        enlaces.style.transition="none"
+        abierto=false
     }
+}
 })
